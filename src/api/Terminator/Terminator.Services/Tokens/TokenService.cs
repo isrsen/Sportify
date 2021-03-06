bd86bd1cb81b79635d9f2429cf8b39020326c501
Ivan Srsen <ivan.srsen@enterwell.net>
2021-03-06 16:02:55 +0100

    Test direct push

--- src/api/Terminator/Terminator.Services/Tokens/TokenService.cs
+++ src/api/Terminator/Terminator.Services/Tokens/TokenService.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
-using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using Terminator.Contracts.Api.Settings;
 using Terminator.Contracts.Services;
